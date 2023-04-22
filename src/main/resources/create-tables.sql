4934b5d673067625f8fffd22905aea74ff972d7e
mcblankenburg <maciej.blankenburg@gmail.com>
2023-04-23 01:29:31 +0200

    created schema for DB and added path to environment variables in application.properties

---
+++ src/main/resources/create-tables.sql
@@ -0,0 +1,6 @@
+CREATE TABLE users (
+                       id INT NOT NULL AUTO_INCREMENT,
+                       first_name VARCHAR(45) NOT NULL,
+                       last_name VARCHAR(45) NOT NULL,
+                       PRIMARY KEY (id)
+);
