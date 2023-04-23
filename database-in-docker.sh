1300ffae212c01dc9212f251aa88928155c7747e
mcblankenburg <maciej.blankenburg@gmail.com>
2023-04-23 02:40:26 +0200

    problematic volume deleted

--- database-in-docker.sh
+++ database-in-docker.sh
@@ -3,6 +3,5 @@ docker run -d \
 -e MYSQL_ROOT_PASSWORD=password \
 -e MYSQL_DATABASE=mydb \
 -v $PWD/src/main/resources/db/:/docker-entrypoint-initdb.d \
--v $PWD/src/main/resources/volume/:/var/lib/mysql \
 -p 3306:3306 \
 mysql
